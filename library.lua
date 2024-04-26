efc65cd9a582e2197d507399d40925d2eff83a21
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-04-26 21:49:35 +0100

    Update library.lua

--- library.lua
+++ library.lua
@@ -29,7 +29,47 @@ function library:CreateMainWindow(args)
 	checkpyMain.BorderSizePixel = 0
 	checkpyMain.Position = UDim2.new(0, 15, 0, 15)
 	checkpyMain.Size = UDim2.new(0, 270, 0, 480)
-	if args.Drag then checkpyMain.Draggable = true checkpyMain.Active = true end
+	if args.Drag then 
+		local UserInputService = game:GetService("UserInputService")
+		
+		local gui = checkpymain
+		
+		local dragging
+		local dragInput
+		local dragStart
+		local startPos
+		
+		local function update(input)
+			local delta = input.Position - dragStart
+			gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		end
+		
+		gui.InputBegan:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				dragging = true
+				dragStart = input.Position
+				startPos = gui.Position
+		
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragging = false
+					end
+				end)
+			end
+		end)
+		
+		gui.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		
+		UserInputService.InputChanged:Connect(function(input)
+			if input == dragInput and dragging then
+				update(input)
+			end
+		end)
+	end
 	
 	TopBar.Name = "checkpyMain"
 	TopBar.Parent = checkpyMain
